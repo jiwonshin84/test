f9ad9400b9133e907bbc181e4f0289d6d669517b
jiwonshin84 <jiwonshin84@gmail.com>
2025-04-23 13:14:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ st.markdown(
 )
 # Define the pages
 main_page = st.Page("main_page.py", title="Main Page", icon="🎈")
-page_2 = st.Page("Pages/page_2.py", title="Page 2", icon="❄️")
+page_2 = st.Page("page_2.py", title="Page 2", icon="❄️")
 page_3 = st.Page("page_3.py", title="Page 3", icon="🎉")
 
 # Set up navigation
